40112af0272670d6ef11c6053d12cb368d4b5fe8
Peter Wang <65141087+peterw712@users.noreply.github.com>
2021-03-13 14:39:04 -0500

    more functionality

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,26 +1,40 @@
+// setting elements as variables
+
 var timerEl = document.getElementById('countdown');
 var startBtn = document.getElementById('start');
-var wrongBtn = document.getElementById('wrong-answer');
+var wrongBtn1 = document.getElementById('wrong-answer1');
+var wrongBtn2 = document.getElementById('wrong-answer2');
+var wrongBtn3 = document.getElementById('wrong-answer3');
 var rightBtn = document.getElementById('right-answer');
 var nextBtn = document.getElementById('next-question');
 var question = document.getElementById('question');
+var result = document.getElementById('result')
+var score = document.getElementById('score')
+
+// only display start button on load
 question.style.display = "none"
-wrongBtn.style.display = "none"
+wrongBtn1.style.display = "none"
+wrongBtn2.style.display = "none"
+wrongBtn3.style.display = "none"
 rightBtn.style.display = "none"
 nextBtn.style.display = "none"
+// next button starts out disabled for first question
 nextBtn.disabled = true
 
+// to hide start button once it is pressed
 function hideButton() {
     startBtn.style.display = "none";
 }
 
+// display all buttons on start
 function start() {
-    var buttonArray = [rightBtn, wrongBtn, nextBtn];
-    for (var i = 0; i < buttonArray.length; i++)
-    {buttonArray[i].style.display = "block";}
+    var allButtons = [rightBtn, wrongBtn1, wrongBtn2, wrongBtn3, nextBtn];
+    for (var i = 0; i < allButtons.length; i++)
+    {allButtons[i].style.display = "block";}
     question.style.display = "block";
 }
 
+// timing functions
 var timeLeft = 75;
 
 function decrementByFifteen() {
@@ -43,45 +57,70 @@ function countdown() {
     }, 1000);
   }
 
+// if an answer is incorrect
   function displayWrongMessage() {
-    document.getElementById("result").innerHTML = "Incorrect.";
+    result.innerHTML = "Incorrect.";
   }
-
+// if an answer is correct
   function displayRightMessage() {
-    document.getElementById("result").innerHTML = "Correct!";
+    result.innerHTML = "Correct!";
   }
-  
-  var wrongAnswers = ['2b', '3a', '4b', '5a']
-
-  var rightAnswers = ['2a', '3b', '4a', '5b']
-
-  var questions = ['can you get this right? 2', 'can you get this right? 3', 'can you get this right? 4', 'can you get this right? 5']
-  
-  var i = 0
-
-  var randomOrder = (Math.floor(Math.random() * 4));
-  var randomOrder2 = (Math.floor(Math.random() * 4));
-
+  // arrays for possible answers for each question
+  var wrongAnswers1 = ['1a', '2a', '3a', '4a']
+  var wrongAnswers2 = ['1b', '2b', '3b', '4b']
+  var wrongAnswers3 = ['1c', '2c', '3c', '4c']
+  var rightAnswers = ['correct1', 'correct2', 'correct3', 'correct4']
+// array for questions
+  var questions = ['can you get this right? 1', 'can you get this right? 2', 'can you get this right? 3', 'can you get this right? 4']
+// randomize choice order
   function randomizeOrder() {
-  wrongBtn.style.order = randomOrder.toString();
-  rightBtn.style.order = randomOrder2.toString();}
-
+    var randomOrder = (Math.floor(Math.random() * 8));
+    var randomOrder2 = (Math.floor(Math.random() * 8));
+    var randomOrder3 = (Math.floor(Math.random() * 8));
+    var randomOrder4 = (Math.floor(Math.random() * 8));
+  
+    wrongBtn1.style.order = randomOrder.toString();
+    wrongBtn2.style.order = randomOrder2.toString();
+    wrongBtn3.style.order = randomOrder3.toString();
+    rightBtn.style.order = randomOrder4.toString();
+}
   randomizeOrder();
+// change question when next question is pressed
+  var i = 0;
 
   function nextQuestion() {
-      wrongBtn.innerHTML = wrongAnswers[i];
+      wrongBtn1.innerHTML = wrongAnswers1[i];
+      wrongBtn2.innerHTML = wrongAnswers2[i];
+      wrongBtn3.innerHTML = wrongAnswers3[i];
       rightBtn.innerHTML = rightAnswers[i];
-
       question.innerHTML = questions[i];
-      randomizeOrder();
-      i++
-  }
+      i++;
+      // display score and hide other elements after the last question
+      if (i > 4) {
+        score.innerHTML = timeLeft.toString();
+        question.style.display = 'none';
+        wrongBtn1.style.display = 'none';
+        wrongBtn2.style.display = 'none';
+        wrongBtn3.style.display = 'none';
+        rightBtn.style.display = 'none';
+        nextBtn.style.display = 'none';
+        result.style.display = 'none'
+        timerEl.style.display = 'none'
+      }
+    }
 
+var choices = [rightBtn, wrongBtn1, wrongBtn2, wrongBtn3];
+
+//enabling choices and disabling next button, or disabling choices and enabling next button
 
 function disableChoices() {
-    var buttonArray = [rightBtn, wrongBtn];
-    for (var i = 0; i < buttonArray.length; i++)
-    {buttonArray[i].disabled = true; }
+    for (var i = 0; i < choices.length; i++)
+    {choices[i].disabled = true; }
+}
+
+function enableChoices() {
+    for (var i = 0; i < choices.length; i++)
+    {choices[i].disabled = false; }
 }
 
 function disableNext() {
@@ -92,26 +131,40 @@ function enableNext() {
     nextBtn.disabled = false;
 }
 
-function enableChoices() {
-    var buttonArray = [rightBtn, wrongBtn];
-    for (var i = 0; i < buttonArray.length; i++)
-    {buttonArray[i].disabled = false; }
+//functions to be executed on click
+
+function startFunctions() {
+    countdown();
+    start();
+    hideButton();
 }
 
-  startBtn.addEventListener("click", countdown);
-  startBtn.addEventListener("click", start);
-  startBtn.addEventListener("click", hideButton);
-  wrongBtn.addEventListener("click", displayWrongMessage);
-  wrongBtn.addEventListener("click", decrementByFifteen);
-  wrongBtn.addEventListener("click", disableChoices);
-  wrongBtn.addEventListener("click", enableNext);
-  rightBtn.addEventListener("click", displayRightMessage);
-  rightBtn.addEventListener("click", disableChoices);
-  rightBtn.addEventListener("click", enableNext);
-  nextBtn.addEventListener("click", nextQuestion);
-  nextBtn.addEventListener("click", enableChoices);
-  nextBtn.addEventListener("click", disableNext);
-  nextBtn.addEventListener("click", randomizeOrder);
+function wrongFunctions() {
+    displayWrongMessage();
+    decrementByFifteen();
+    disableChoices();
+    enableNext();
+}
+
+function rightFunctions() {
+    displayRightMessage();
+    disableChoices();
+    enableNext();
+}
+
+function nextFunctions() {
+    nextQuestion();
+    enableChoices();
+    disableNext();
+    randomizeOrder();
+}
+
+  startBtn.addEventListener("click", startFunctions);
+  wrongBtn1.addEventListener("click", wrongFunctions);
+  wrongBtn2.addEventListener("click", wrongFunctions);
+  wrongBtn3.addEventListener("click", wrongFunctions);
+  rightBtn.addEventListener("click", rightFunctions);
+  nextBtn.addEventListener("click", nextFunctions);
 
 
   
